7126b246475f02969fea2c68efe3a15301d5ca9a
ivan heinemeier <ivan.heinemeier@gmail.com>
2023-01-18 11:56:13 +0100

    solved day 3, part 1

---
+++ solutions/day_3.py
@@ -0,0 +1,19 @@
+"""
+Day 3 solution. both for step 1 and 2
+"""
+
+from pathlib import Path
+
+
+ALPHABETS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+r = 0
+for backpack in Path('input_data/input_d3.txt').read_text().split("\n"):
+    room1 = set(backpack[:len(backpack)//2])
+    room2 = set(backpack[len(backpack)//2:])
+
+    l = str(room1.intersection(room2))[2]  # 0-indexing
+
+    r += ALPHABETS.index(l) + 1
+
+print(f"solution day 3, part 1: {r}")
