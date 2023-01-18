cce589c3ef9c313c32b323daf11d86f3d517ad6a
ivan heinemeier <ivan.heinemeier@gmail.com>
2023-01-18 11:57:30 +0100

    moved comment

--- solutions/day_3.py
+++ solutions/day_3.py
@@ -12,8 +12,8 @@ for backpack in Path('input_data/input_d3.txt').read_text().split("\n"):
     room1 = set(backpack[:len(backpack)//2])
     room2 = set(backpack[len(backpack)//2:])
 
-    l = str(room1.intersection(room2))[2]  # 0-indexing
+    l = str(room1.intersection(room2))[2]
 
-    r += ALPHABETS.index(l) + 1
+    r += ALPHABETS.index(l) + 1  # 0-indexing
 
 print(f"solution day 3, part 1: {r}")
