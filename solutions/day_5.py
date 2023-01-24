8811125c8f601f13778a52c895c6c549d7e0d4fb
ivan heinemeier <ivan.heinemeier@gmail.com>
2023-01-24 10:31:26 +0100

    added quick restart to the button R.

---
+++ solutions/day_5.py
@@ -0,0 +1,12 @@
+"""
+Day 5 solution. Both for step 1 and 2.
+"""
+
+from pathlib import Path
+import re
+
+
+input = Path('input_data/input_d5.txt').read_text().split("\n")
+
+for l in input[:10]:
+
